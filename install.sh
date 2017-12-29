b5912d1f1e6ace2b7efbb9566d7d00065b64c112
AtjonTV <admin.atjontv@atvg-studios.at>
2017-12-29 17:02:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ apt upgrade -y
 apt install -y python
 apt install -y python-pip
 apt install -y unzip
-apt install -y screen
 cd /tmp
 wget https://github.com/OlympicCode/vHackXTBot-Python/archive/master.zip
 unzip master.zip
@@ -13,5 +12,4 @@ pip install -r requirements.txt
 clear
 echo "The bot was installed on your system!"
 echo "Now open the 'config.py' in '~/vhack-bot' and enter your credentials"
-echo "Write 'python ~/vhack-bot/main.py' to start in current terminal session or"
-echo "run 'sh ~/vhack-bot/start-vscreen.sh' for opening the bot in a new terminal session"
+echo "Write 'python ~/vhack-bot/main.py' to start the bot"
